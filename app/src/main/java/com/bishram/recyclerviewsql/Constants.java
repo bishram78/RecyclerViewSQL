32937ee2eb171f63a2866061ebede625a51252d4
bishram78 <bishram.munda78@gmail.com>
2018-04-24 06:57:52 +0530

    Constants.java file modified

--- app/src/main/java/com/bishram/recyclerviewsql/Constants.java
+++ app/src/main/java/com/bishram/recyclerviewsql/Constants.java
@@ -1,4 +1,14 @@
 package com.bishram.recyclerviewsql;
 
 public class Constants {
+
+    //DATABASE PROPERTIES
+    static final int DB_VERSION = 1;
+    static final String DB_NAME = "d_DB";
+    static final String TB_NAME = "d_TB";
+
+    //COLUMNS OF THE TABLE
+    static final String ROW_ID = "id";
+    static final String COLUMN_NAME = "name";
+    static final String COLUMN_POSITION = "position";
 }
