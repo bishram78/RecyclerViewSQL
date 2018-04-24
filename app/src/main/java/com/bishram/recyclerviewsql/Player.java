296030002731d5b72de0a1dc8f0b578662bc0960
bishram78 <bishram.munda78@gmail.com>
2018-04-24 06:50:27 +0530

    Player.java file modified

--- app/src/main/java/com/bishram/recyclerviewsql/Player.java
+++ app/src/main/java/com/bishram/recyclerviewsql/Player.java
@@ -1,4 +1,47 @@
 package com.bishram.recyclerviewsql;
 
 public class Player {
+    private int id;
+    private int image;
+    private String name;
+    private String position;
+
+    public Player(int id, String name, String position, int image) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
